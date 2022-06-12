9ae08f1ac10ff6cf460170331fa895992dee9c15
Jonas Van Hastel <jonas.van.hastel@onplo.com>
2022-06-12 16:06:45 +0200

    small update

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -3,6 +3,11 @@ package com.gildedrose;
 class GildedRose {
     Item[] items;
 
+    static final String CONJURED = "Aged Brie";
+    static final String AGED_BRIE = "Aged Brie";
+    static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
     public GildedRose(Item[] items) {
         this.items = items;
     }
@@ -10,39 +15,47 @@ class GildedRose {
     public void updateQuality() {
 
         for (int i = 0; i < items.length; i++) {
+
+            // The Quality of an item is never negative
             if (items[i].quality < 0) {
                 items[i].quality = 0;
                 continue;
             }
 
-            if (items[i].name.contains("Conjured")) {
-                if (items[i].quality > 0 && items[i].quality < 51) {
+            // "Conjured" items degrade in Quality twice as fast as normal items
+            if (items[i].name.contains(CONJURED)) {
+                if (items[i].quality < 50) {
                     items[i].quality -= 2;
                 }
-            } else if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0 && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+
+            } else if (!items[i].name.equals(AGED_BRIE)
+                    && !items[i].name.equals(BACKSTAGE_PASSES)) {
+                if (!items[i].name.equals(SULFURAS)) {
                     items[i].quality -= 1;
                 }
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality += 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert") && items[i].sellIn < 11
-                            && items[i].quality < 50) {
+                    if (items[i].name.equals(BACKSTAGE_PASSES)) {
                         items[i].quality += 1;
+
+                        if (items[i].sellIn < 11) {
+                            items[i].quality += 1;
+                        }
+
+                        if (items[i].sellIn < 6) {
+                            items[i].quality += 1;
+                        }
                     }
+
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0 && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (!items[i].name.equals(AGED_BRIE)) {
+                    if (!items[i].name.equals(BACKSTAGE_PASSES)) {
+                        if (!items[i].name.equals(SULFURAS)) {
                             items[i].quality -= 1;
                         }
                     } else {
@@ -53,6 +66,11 @@ class GildedRose {
                         items[i].quality += 1;
                     }
                 }
+            }
+
+            // "Sulfuras", being a legendary item, never has to be sold
+            if (!items[i].name.equals(SULFURAS)) {
+                items[i].sellIn -= 1;
             }
         }
     }
